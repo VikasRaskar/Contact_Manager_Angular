c6b2ba5af20cd763c96318ed393b6e82da5fa173
VikasRaskar <vikasraskar9273@gmail.com>
2022-05-26 16:12:23 +0530

    new file

---
+++ src/app/models/iContact.ts
@@ -0,0 +1,11 @@
+export interface iContact{
+    id ? : string;
+    name : string;
+    email : string;
+    photo : string;
+    mobile : string;
+    compony : string;
+    title : string;
+    groupId : string;
+
+}
